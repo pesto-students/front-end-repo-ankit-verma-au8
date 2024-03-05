db9b45fb39ba8c7c571fe232ed4c266f372a0a67
mehra-sourav <1997mehra@gmail.com>
2024-03-05 16:44:47 +0530

    Changed environment variable location

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.BACKEND_BASE_URL,
+  baseURL: import.meta.env.VITE_BACKEND_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },

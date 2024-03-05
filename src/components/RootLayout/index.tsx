8ff0b39c7c59a500a3b6619288b0a7e54424f639
Sourav Mehra <36883648+mehra-sourav@users.noreply.github.com>
2024-03-05 20:58:09 +0530

    Added Sidebar to the app (#8)
    * Added SideNav to the app
    
    * Updated main section to grow and shrink with sidenav size
    
    * Added mobile view to sidebar and updated navbar with menu icon
    
    * Fixed TypeScript build issues

--- src/components/RootLayout/index.tsx
+++ src/components/RootLayout/index.tsx
@@ -1,25 +1,37 @@
+import { useState } from "react";
 import { Container } from "@mui/material";
 import Navbar from "@/components/RootLayout/NavBar";
+import SideNav from "../SideNav";
+import MainSection from "../MainSection";
 import { Outlet } from "react-router-dom";
 
 const RootLayout = () => {
+  const [isSideNavOpen, setSideNavOpen] = useState(true);
+
+  const toggleDrawer = () => {
+    setSideNavOpen((oldState) => !oldState);
+  };
+
   return (
-    <div style={{ height: "100%" }}>
-      <Navbar />
-      <Container
-        component="main"
-        maxWidth={false}
-        sx={{
-          backgroundColor: "background.main",
-          padding: "10px 50px",
-          border: "1px solid red",
-          display: "grid",
-          alignItems: "start",
-          height: "100%",
-        }}
-      >
-        <Outlet />
-      </Container>
+    <div style={{ height: "100%", display: "flex" }}>
+      <SideNav open={isSideNavOpen} toggleDrawer={toggleDrawer} />
+      <MainSection drawerOpen={isSideNavOpen}>
+        <Navbar drawerOpen={isSideNavOpen} toggleDrawer={toggleDrawer} />
+        <Container
+          component="main"
+          maxWidth={false}
+          sx={{
+            backgroundColor: "background.main",
+            padding: "10px 50px",
+            border: "1px solid red",
+            display: "grid",
+            alignItems: "start",
+            height: "100%",
+          }}
+        >
+          <Outlet />
+        </Container>
+      </MainSection>
     </div>
   );
 };
---
+++ src/components/SideNav/index.tsx
@@ -0,0 +1,136 @@
+import {
+  Drawer as MUIDrawer,
+  IconButton,
+  Divider,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import ExpenseIcon from "@mui/icons-material/FormatListBulleted";
+import BudgetIcon from "@mui/icons-material/RequestQuote";
+import SettingsIcon from "@mui/icons-material/Settings";
+import { styled, Theme, CSSObject } from "@mui/material/styles";
+
+interface Props {
+  open: boolean;
+  toggleDrawer: () => void;
+}
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: "flex-end",
+}));
+
+const openedMixin = (theme: Theme): CSSObject => ({
+  width: 240,
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflowX: "hidden",
+});
+
+const closedMixin = (theme: Theme): CSSObject => ({
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: "hidden",
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up("sm")]: {
+    width: `calc(${theme.spacing(8)} + 1px)`,
+  },
+});
+
+const Drawer = styled(MUIDrawer)(({ theme, open }) => ({
+  width: 240,
+  flexShrink: 0,
+  whiteSpace: "nowrap",
+  boxSizing: "border-box",
+  ...(open && {
+    ...openedMixin(theme),
+    "& .MuiDrawer-paper": openedMixin(theme),
+  }),
+  ...(!open && {
+    ...closedMixin(theme),
+    "& .MuiDrawer-paper": closedMixin(theme),
+  }),
+}));
+
+const SideNav = ({ open, toggleDrawer }: Props) => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+
+  const pageList = [
+    { text: "Overview", icon: <DashboardIcon /> },
+    { text: "Expenses", icon: <ExpenseIcon /> },
+    { text: "Budgets", icon: <BudgetIcon /> },
+    { text: "Settings", icon: <SettingsIcon /> },
+  ];
+
+  const DrawerContent = (
+    <>
+      <DrawerHeader>
+        {!isMobile && (
+          <IconButton onClick={toggleDrawer}>
+            {open ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        )}
+      </DrawerHeader>
+      <Divider />
+      <List>
+        {pageList.slice(0, pageList.length - 1).map(({ text, icon }) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>{icon}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <List sx={{ mt: "auto" }}>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>{pageList[pageList.length - 1].icon}</ListItemIcon>
+            <ListItemText primary={pageList[pageList.length - 1].text} />
+          </ListItemButton>
+        </ListItem>
+      </List>
+      <Divider />
+    </>
+  );
+
+  return (
+    <>
+      {isMobile ? (
+        <Drawer
+          variant="temporary"
+          open={open}
+          onClose={toggleDrawer}
+          ModalProps={{
+            keepMounted: true,
+          }}
+        >
+          {DrawerContent}
+        </Drawer>
+      ) : (
+        <Drawer variant="permanent" open={open}>
+          {DrawerContent}
+        </Drawer>
+      )}
+    </>
+  );
+};
+
+export default SideNav;
